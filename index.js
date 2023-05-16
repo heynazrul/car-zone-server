a1ba653d6805f96777694bed261117e4ee678430
Nazrul <heynazrul@gmail.com>
2023-05-16 15:53:11 +0600

    added jwt token verifying function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
@@ -20,6 +21,26 @@ const client = new MongoClient(uri, {
   },
 });
 
+const verifyJWT = (req, res, next) => {
+  // console.log('hitting verify JWT');
+  // console.log(req.headers.authorization);
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access!' });
+  }
+
+  const token = authorization.split(' ')[1];
+  // console.log('token inside verify jwt', token);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+    if (error) {
+      return res.status(403).send({ error: true, message: 'unauthorized access!' });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -28,6 +49,18 @@ async function run() {
     const serviceCollection = client.db('carZone').collection('services');
     const appointmentCollection = client.db('carZone').collection('appointments');
 
+    // jwt
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      console.log(user);
+
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1h',
+      });
+      res.send({ token });
+    });
+
+    // Service route
     app.get('/services', async (req, res) => {
       const cursor = serviceCollection.find();
       const result = await cursor.toArray();
@@ -54,10 +87,16 @@ async function run() {
       res.send(result);
     });
 
-    // appointments
+    // appointments route
 
-    app.get('/appointments', async (req, res) => {
-      console.log(req.query);
+    app.get('/appointments', verifyJWT, async (req, res) => {
+      // console.log(req.headers.authorization);
+      const decoded = req.decoded;
+      console.log('came back after verify', decoded);
+
+      if (decoded.email !== req.query.email) {
+        return res.status(403).send({ error: 1, message: 'forbidden access!' });
+      }
       let query = {};
       if (req.query?.email) {
         query = { email: req.query.email };
